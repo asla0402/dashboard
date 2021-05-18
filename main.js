4c3403b505cfd008882fb980ffd5593d40b2914b
Aslak <aslakszs@hotmail.com>
2021-05-19 01:04:16 +0200

    html struktur er blevet sat op. styling/struktur er blevet sat op. javascript er så småt kommet i gang

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
+
+"use strict"
+
 import './style.scss'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+window.addEventListener("DOMContentLoaded", get);
+
+const endpoint = "https://foobar-vas.herokuapp.com/";
+
+const lastId = null;
+
+function get() {
+  fetch(endpoint, {
+      method: "get",
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+      },
+  }) 
+  .then((e) => e.json())
+  .then(addOrder);
+  //setInterval(get, 5000);
+  }
+
+
+  function addOrder(data) {
+    //init
+    //emty object to insert the data 
+    const sold = {
+    };
+
+    let queue = data.queue;
+    
+    //everytime we recieve new data
+    const newOrders = queue.filter(order => order.id > lastId)
+    lastId = queue[queue.length-1].id
+    
+    newOrders.array.forEach(order=> {
+      order.order.forEach(beer=> {
+        sold[beer]++
+      })
+    });  
+  }
+
+
+
+
+
+//match images med navne fra json
+
+//udregn nr. 1, nr. 2 og nr. 3 ud fra sold-objektet 
+
+//klon til html templates
+//if nr. 1 klon til template 1, else if nr. 2 klon til template 2, else if nr 3. klon til template 3 
+
+//gør barene dynamiske 
